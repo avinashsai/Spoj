d2d06b4f7f344a30191c8ce964fcfac0f5247da9
Avinash Madasu <avinash.sai001@gmail.com>
2017-11-18 15:48:13 +0530

    Added Solutions

--- Ad-hoc/LASTDIG.cpp
+++ Ad-hoc/LASTDIG.cpp
@@ -1,23 +1,27 @@
 #include<bits/stdc++.h>
 #define ll long long int
 using namespace std;
-ll power(ll a,ll b)
-{
-	if(b==0)
-		return a%10;
-	else if(b%2==0)
-		return (power(a,b/2)%10 * power(a,b/2)%10)%10;
-	else
-		return (a%10 * power(a,b/2)%10 * power(a,b/2)%10)%10;
-}
+int a[9][4]={{1,1,1,1},{2,4,8,6},{3,9,7,1},{4,6,4,6},{5,5,5,5},{6,6,6,6},{7,9,3,1},{8,4,2,6},{9,1,9,1}};
 int main()
 {
-    ll a,b,no;
+    ll b,no,c;
     cin>>no;
     while(no--)
     {
-    	cin>>a>>b;
-        cout<<power(a,b)<<endl;
+    	cin>>c>>b; 
+        if(b==0)
+            cout<<"1";
+        else if(c%10==0)
+            cout<<"0";
+        else
+        {
+        int k=c%10;
+        int p=b%4;
+        if(p==0)
+            p=4;
+        cout<<a[k-1][p-1];
+    }
+    cout<<endl;
     }
     return 0;
 }
